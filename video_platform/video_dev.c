d9f134bdf8ab15081069e1bc08dc3a1465902b66
a_qian <3470140745@qq.com>
2025-08-07 22:32:22 +0800

    更新注释

--- video_platform/video_dev.c
+++ video_platform/video_dev.c
@@ -10,40 +10,27 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 
-// 定义一个唯一的设备/驱动匹配名称，这是两者之间唯一的联系。
 #define DRIVER_NAME "vcam_plat"
 
-/**
- * @brief 设备释放回调函数。
- * @details 当设备的最后一个引用被释放时，此函数被调用。对于一个
- * 静态分配的虚拟设备，这个函数可以为空。
- */
 static void vcam_plat_device_release(struct device *dev)
 {
     pr_info("平台设备 %s 已释放\n", DRIVER_NAME);
 }
 
-// 静态定义一个平台设备实例。
 static struct platform_device vcam_platform_device = {
-    .name = DRIVER_NAME, // 设备的名称，必须与驱动的名称匹配
+    .name = DRIVER_NAME, // 设备的名称
     .id   = -1,          // -1 表示只有一个该类型的设备
     .dev = {
         .release = vcam_plat_device_release,
     },
 };
 
-/**
- * @brief 模块初始化函数。
- */
+
 static int __init vcam_device_init(void)
 {
     pr_info("注册虚拟摄像头平台设备 '%s'...\n", DRIVER_NAME);
     return platform_device_register(&vcam_platform_device);
 }
-
-/**
- * @brief 模块退出函数。
- */
 static void __exit vcam_device_exit(void)
 {
     pr_info("注销虚拟摄像头平台设备 '%s'...\n", DRIVER_NAME);
